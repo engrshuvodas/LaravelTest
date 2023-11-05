f8280b136ff77cc065863d3667145decda9ad371
Engr Shuvo Das <136590233+engrshuvodas@users.noreply.github.com>
2023-11-05 20:47:52 +0600

    Add files via upload

---
+++ storage/framework/views/09305180a9b42bec2f43bf2318b4a025.php
@@ -0,0 +1,3 @@
+this is about us page.
+<br>
+my name is <?php echo e($name); ?> and my age is <?php echo e($age); ?><?php /**PATH C:\xampp\htdocs\LMSPOINT\LMS\resources\views/aboutus.blade.php ENDPATH**/ ?>
